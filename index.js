fc336ee5ac61a8a6d04463b58003100d8042cd27
ToonZLGamer <iamkenneth63@gmail.com>
2021-10-27 19:25:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ client.on("message", message => {
         if (message.author.id !== '724423864611766294') return;
          const args = message.content.slice().trim().split(/ +/g );
          const command = args.shift().toLowerCase();
-         if (message.content === 't.supernatural') {
-     message.channel.send(`https://open.spotify.com/track/73icgGWQ5Zy89lwam6dIms?si=36fb5b68a20a41e7`);
+         if (message.content === '@Tokyo Machine#1721') {
+     message.channel.send(`a`);
         }
       });
 
