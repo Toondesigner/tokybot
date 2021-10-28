c54f225fa52ab7382db7b8a515ef2dc393169171
ToonZLGamer <iamkenneth63@gmail.com>
2021-10-27 18:22:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -164,4 +164,4 @@ client.on("message", message => {
        message.channel.send(embed)
    }
 });
-client.login('OTAyMjkyNzM4NDkyNDg5Nzc4.YXcTmA.XZcDY-NVxljRnk8VEHdGnMPI41U')
+client.login('OTAyMjkyNzM4NDkyNDg5Nzc4.YXcTmA.XZcDY-NVxljRnk8VEHdGnMPI41U');
