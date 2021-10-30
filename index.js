c6e3b0af2cdae7a96e45393903768beb794074c2
ToonZLGamer <iamkenneth63@gmail.com>
2021-10-29 23:03:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,22 @@ activity: {
         
             message.channel.send(embed);
         }
-        };
+      } else if (message.content.startsWith(prefix+'avatar')) {
+      var server = message.guild;
+  
+const embed = new Discord.MessageEmbed()
+    .setThumbnail(server.iconURL())
+    .setAuthor(server.name, server.iconURL())
+    .addField('ID', server.id, true)
+    .addField('Region', server.region, true)
+    .addField('Creado el', server.joinedAt.toDateString(), true)
+    .addField('Dueño del Servidor', server.owner.user.tag +'('+server.owner.user.id +')', true)
+    .addField('Miembros', server.memberCount, true)
+    .addField('Roles', server.roles.size, true)
+    .setColor(0x66b3ff)
+    
+message.channel.send(embed);
+      }
 });
 //comandos admins
 //comandos solo por id
